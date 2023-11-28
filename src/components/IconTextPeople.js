ff9642f3995dc8ccb4a498fafad66a85b148ae59
bqqmf <39626552+bqqmf@users.noreply.github.com>
2023-11-27 23:46:05 -0800

    add new pannels

--- src/components/IconTextPeople.js
+++ src/components/IconTextPeople.js
@@ -1,17 +1,18 @@
 import kind from '@enact/core/kind';
 import BodyText from '@enact/sandstone/BodyText';
-import Image from '@enact/sandstone/Image';
+
+import LecturePeople from '../asset/images/lecture_people.svg';
 
 
-const IconText = kind({
-	name: 'IconText',
+const IconTextTime = kind({
+	name: 'IconTextTime',
 
-	render: ({ src, text }) => (
+	render: ({ text }) => (
 		<div style={{ display: 'flex', margin: '25px', alignItems: 'center', border: '2px solid', borderColor: 'red' }}>
-			<Image src={src} style={{ width: '48px', height: '48px', border: '2px solid', borderColor: 'green' }} />
+			<img src={LecturePeople} style={{ width: '50px', height: '50px', border: '2px solid', borderColor: 'green' }} alt="" />
 			<BodyText style={{ fontSize: '40px', textAlign: 'center', marginTop: '25px', border: '2px solid', borderColor: 'green' }}>{text}</BodyText>
 		</div>
 	)
 });
 
-export default IconText;
+export default IconTextTime;
