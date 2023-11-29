357740d151d27bfedf8324de793fd2981937a112
jonghyunhub <chuwhdgus@gmail.com>
2023-11-29 11:34:58 +0900

    feat : Image 컴포넌트 의존성 추가

--- src/components/dash-board/Header.js
+++ src/components/dash-board/Header.js
@@ -1,12 +1,13 @@
 import kind from '@enact/core/kind';
 import styles from './Header.module.css';
+import Image from '@enact/agate/Image';
 
 const Header = kind({
     name: 'Header',
     
     render: ()=>(
         <header className={styles.header}>
-            <img src="../src/asset/ssu_logo.png" alt="Logo" className={styles.logo} />
+            <Image src="src/asset/ssu_logo.png" alt="Logo" className={styles.logo} />
         </header>
     )
 });
