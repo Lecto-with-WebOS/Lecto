27a7f8ceed3a61f98c6899ad978eb325a71dbc78
jonghyunhub <chuwhdgus@gmail.com>
2023-11-29 12:04:22 +0900

    feat :대시보드 헤더 컴포넌트 구현

--- src/components/dash-board/Header.js
+++ src/components/dash-board/Header.js
@@ -1,13 +1,13 @@
 import kind from '@enact/core/kind';
 import styles from './Header.module.css';
-import Image from '@enact/agate/Image';
+import ssu_logo from '../../asset/ssu_logo.png';
 
 const Header = kind({
     name: 'Header',
     
     render: ()=>(
         <header className={styles.header}>
-            <Image src="src/asset/ssu_logo.png" alt="Logo" className={styles.logo} />
+            <img src={ssu_logo} alt="Logo" className={styles.logo} />
         </header>
     )
 });
