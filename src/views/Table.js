f85cdd6c50d5acd6cee96ee7261de26dab2aff11
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-12-05 18:09:54 +0900

    feat: add link to main

--- src/views/Table.js
+++ src/views/Table.js
@@ -1,31 +1,42 @@
 import React from "react";
 import styles from "./Table.module.css";
+import ssung from "../asset/images/table_ssung.svg";
+import goBack from "../asset/images/table_back.svg";
+import { Link } from "@enact/ui/Routable";
 
 const Table = () => {
-  const timetable = [
-    [null, 1, null, null, 2, 3, 4, 4, null, null],
-    [null, 5, 6, 6, null, 3, 7, null, null, null],
-    [null, 1, 8, null, 2, 9, 9, null, null, null],
-    [null, 5, 10, null, null, 11, 11, 12, 12, 12],
-    [null, null, 13, 13, null, null, 7, null, null, null],
-    [14, 15, 15, 15, null, null, null, null, null, null]
-  ];
+  const days = ["MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const timetable = JSON.parse(window.localStorage.getItem("tableData"));
+
   const transposedData = timetable[0].map((_, colIndex) =>
     timetable.map((row) => row[colIndex])
   );
 
   const detailData = JSON.parse(window.localStorage.getItem("detailData"));
-
+  const getScheduleTime = (index) => {
+    const scheduleTimes = [
+      "08:00-08:50",
+      "09:00-10:15",
+      "10:30-11:45",
+      "12:00-13:15",
+      "13:30-14:45",
+      "15:00-16:15",
+      "16:30-17:45",
+      "18:00-19:15",
+      "19:30-20:45",
+      "21:00-22:15"
+    ];
+    return scheduleTimes[index];
+  };
   return (
     <div className={styles.container}>
       <div className={styles.table}>
         <div className={styles.header}></div>
-        <div className={styles.header}>MON</div>
-        <div className={styles.header}>TUE</div>
-        <div className={styles.header}>WED</div>
-        <div className={styles.header}>THU</div>
-        <div className={styles.header}>FRI</div>
-        <div className={styles.header}>SAT</div>
+        {days.map((day) => (
+          <div key={day} className={styles.header}>
+            {day}
+          </div>
+        ))}
         {transposedData?.map((row, rowIndex) => (
           <React.Fragment key={rowIndex}>
             <div className={styles.time}>
@@ -49,24 +60,12 @@ const Table = () => {
           </React.Fragment>
         ))}
       </div>
+      <Link path="/main" className={styles.btnWrapper}>
+        <img className={styles.ssung} src={ssung} alt="ssung" />
+        <img className={styles.goBack} src={goBack} alt="goBackBtn" />
+      </Link>
     </div>
   );
-};
-
-const getScheduleTime = (index) => {
-  const scheduleTimes = [
-    "08:00-08:50",
-    "09:00-10:15",
-    "10:30-11:45",
-    "12:00-13:15",
-    "13:30-14:45",
-    "15:00-16:15",
-    "16:30-17:45",
-    "18:00-19:15",
-    "19:30-20:45",
-    "21:00-22:15"
-  ];
-  return scheduleTimes[index];
 };
 
 export default Table;
